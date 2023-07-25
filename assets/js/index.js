c3a3c37e997a11e9e222167803bc3cc451f8fc00
Pablo <emilio7b@gmail.com>
2023-07-24 23:10:38 -0400

    Creacion de landing page (frontend)

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+const nombre_usuario=document.getElementById("user_name").value;
+const edad=document.getElementById("age").value;
+const numero_telefono=document.getElementById("displayed_phone_number").value;
+const foto_perfil=document.getElementById("profile_photo");
+const email=document.getElementById("email").value;
+const carino_nombre=document.getElementById("username_carino").value;
+const check_box=document.getElementById("check");
+const btn_registrar=document.getElementById("btn_registrar");
+
+console.log(numero_telefono);
